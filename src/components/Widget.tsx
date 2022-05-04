06ba5044619e74acf9106911549a997d80fb2e3a
João Marcos <joaombcerqueira@gmail.com>
2022-05-03 22:05:09 -0300

    feat: widget form component

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,6 +1,7 @@
 import { ChatTeardropDots } from 'phosphor-react'
 import { useState } from 'react'
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm'
 
 export function Widget() {
 
@@ -11,8 +12,8 @@ export function Widget() {
     // }
 
     return (
-        <Popover className='absolute bottom-5 right-5'>
-            <Popover.Panel>Juba Linda</Popover.Panel>
+        <Popover className='absolute bottom-4 right-4 md:bottom-8 md:right-8 flex flex-col items-end'>
+            <Popover.Panel><WidgetForm /></Popover.Panel>
             
             {/* {isWidgetOpen ? <p>Juba Linda</p> : null} */}
 
